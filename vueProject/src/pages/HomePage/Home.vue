0cc4042f71908380e8867a93ae75623574f0bcd3
jeonggil <jeonggilahn@gmail.com>
2026-04-08 10:20:42 +0900

    feat : homepage setting

--- vueProject/src/pages/HomePage/Home.vue
+++ vueProject/src/pages/HomePage/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>a</div>
+  <button @click="router.push({ name: 'join' })">회원가입</button>
+  <button @click="router.push({ name: 'login' })">로그인</button>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
 
 <style scoped></style>
