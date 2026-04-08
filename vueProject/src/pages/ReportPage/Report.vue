b10d9d5d66d98926058f11a918528e671118327e
zozo9109 <zozo9109@naver.com>
2026-04-08 13:28:11 +0900

    feat report 수정

--- vueProject/src/pages/ReportPage/Report.vue
+++ vueProject/src/pages/ReportPage/Report.vue
@@ -2,6 +2,21 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+
+//chart.js 필요한 모듈만 등록함
+Chart.register(ArcElement, Tooltip, Legend, DatasetController);
+
+const doubleDoughnut = {
+  //1. 데이터 정의(안쪽 원, 바깥쪽 원)
+};
+
+const options = {};
+
+const store = useCounterStore();
+</script>
 
 <style scoped></style>
