b3e03a1cc6b1574ffbe4a51b864ab1e6d39a21ea
zozo9109 <zozo9109@naver.com>
2026-04-09 10:44:21 +0900

    feat : 충돌수정

--- vueProject/src/pages/HomePage/Home.vue
+++ vueProject/src/pages/HomePage/Home.vue
@@ -1,10 +1,6 @@
 <template>
-<<<<<<< HEAD
-  <div></div>
-=======
   <button @click="router.push({ name: 'join' })">회원가입</button>
   <button @click="router.push({ name: 'login' })">로그인</button>
->>>>>>> develop
 </template>
 
 <script setup>
